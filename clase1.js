8cd4f54cc607766d5a468a999479a4f35cf560b7
jorgeduje <jorgeduje4@gmail.com>
2025-03-18 20:46:35 -0300

    clase 1

---
+++ clase1.js
@@ -0,0 +1,23 @@
+// variables
+
+// let o const --> palabras reservadas
+
+// iniciador / nombre / = / valor
+
+// let edad_del_usuario = 32;
+// let edaddelusuario = 32;
+
+let edadDelUsuario = 1500;
+const apellido = "perez";
+
+// tipos de datos
+
+// string --> cadenas de texto
+// number --> numeros enteros o decimales
+// boolean --> los datos verdadero o falso ( true o false )
+
+console.log(apellido);
+console.log(edadDelUsuario);
+
+let esMayor = true;
+let esCasado = false;
