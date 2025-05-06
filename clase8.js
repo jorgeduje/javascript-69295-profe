eb82851f1807d4e5c9d03586dfbf28c23f64ac7c
jorgeduje <jorgeduje4@gmail.com>
2025-05-06 20:54:30 -0300

    se implementa fetching

---
+++ clase8.js
@@ -0,0 +1,54 @@
+// hacer una peticion a una api
+// endpoint y donde va el verbo ( por defecto va el GET )
+const getProducts = async () => {
+  let contenedor = document.getElementById("container-products");
+  contenedor.innerHTML = `<div class="loader"></div>`;
+  let bloqueFinal = document.getElementById("bloque-final");
+  try {
+    let respuesta = await fetch("https://fakestoreapi.com/productss");
+    if (!respuesta.ok) {
+      throw new Error("este es un error custom que ponemos nosotros");
+    }
+    // parsear
+    // "[]" --> "[]".json() --> []
+    let productos = await respuesta.json();
+    // container-products
+
+    setTimeout(() => {
+      contenedor.innerHTML = "";
+
+      let contendioProductos = "";
+
+      productos.forEach((producto) => {
+        contendioProductos += `<div>${producto.title}</div>`;
+      });
+      contenedor.innerHTML = contendioProductos;
+    }, 2000);
+  } catch (error) {
+    console.log(error.message);
+    let esElCustom = error.message.includes(
+      "este es un error custom que ponemos nosotros"
+    );
+    console.log(esElCustom);
+    setTimeout(() => {
+      contenedor.innerHTML = `<h2>Algo salio mal bla erorr: ${
+        esElCustom ? error.message : "ERROR GENERICO"
+      }</h2>`;
+    }, 2000);
+  } finally {
+    bloqueFinal.textContent = "Termino el bloque try catch";
+  }
+};
+
+getProducts();
+
+// ASINCRONICAS
+// "{}" ---> "{}".json() --->{}
+
+// pintar los productos en el frontend
+
+// const pintarProductos = async (productos) => {
+//   let products = await productos;
+//   console.log(products);
+// };
+// pintarProductos(getProducts());
